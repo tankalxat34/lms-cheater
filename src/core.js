8e81f87214721d28bed1a123c53215e88277ff5d
tankalxat34 <tankalxat34@gmail.com>
2022-07-04 20:33:18 +0300

    Работа над стилями и блоками

--- src/core.js
+++ src/core.js
@@ -15,11 +15,29 @@ function addCssToDom(url) {
 }
 
 addCssToDom("https://raw.githubusercontent.com/tankalxat34/lms-cheeter/main/src/lms-styles.css")
+console.log("Стили успешно загружены!")
 
+// прочитать стили
 let successAlertSection = document.getElementById("block-region-side-pre")
 let alertDiv = document.createElement("section")
-alertDiv.className = "block card mb-3 success-cheeter-alert"
-alertDiv.innerHTML = "<h5 class='card-title d-inline'>LMS Cheeter успешно подключен</h5>"
+alertDiv.className = " block_online_users block  card mb-3"
+alertDiv.innerHTML = 
+    `
+<div class="card-body p-3" id="cheeter-alert">
+    <h5 class="card-title d-inline">Расширение LMS Cheeter загружено</h5>
+
+    <div class="card-text content mt-3">
+        <div class="info" style="text-align: justify">
+            <p>Данное расширение позволит вам успешнее проходить онлайн-тесты, которые есть в СДО РАНХиГС. Оно подсвечивает вероятные ответы на каждый вопрос теста, если на конкретный вопрос уже были даны ответы другими пользователями расширения.</p>
+            <ul>
+                <li><a href="https://vk.com/tankalxat34">Автор расширения</a></li>
+            </ul>
+        </div>
+        <div class="footer"></div>
+    </div>
+</div>
+`
 successAlertSection.append(alertDiv)
 
+// вывод в консоль надписи об успешной загрузке расширения
 console.log("LMS Cheeter успешно подключен!")
